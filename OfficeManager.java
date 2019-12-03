71700eb2e4bd83373108fb122bb91fdf498eb968
bweems1986 <bweems1986@gmail.com>
2019-12-03 14:48:32 -0500

    Add files via upload

--- OfficeManager.java
+++ OfficeManager.java
@@ -1,21 +1,18 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class OfficeManager extends Warehouse {
-
-    Warehouse partWarehouse = new Warehouse();
 
     public OfficeManager() {
 
     }
 
+    /**
+     * this method allows the office manager to enter a part by name to get the information of said part
+     * @param partName
+     */
     public void examinePartName(String partName) {//office manager can enter a part name and receives part info
-        //super.findPart(partName);//figure out how to call superclass method and add additional
         boolean found = false;
 
         for (int i = 0; bikeParts.size() > i; i++) {
@@ -32,8 +29,11 @@ public class OfficeManager extends Warehouse {
         }
     }
 
+    /**
+     * this method allows the office manager to enter a part by number to the the information for said part
+     * @param partNumber
+     */
     public void examinePartNumber(int partNumber) {//office manager can enter part number and get part info
-        //super.findPart(partNumber);
         boolean found = false;
 
         for (int i = 0; bikeParts.size() > i; i++) {
@@ -51,6 +51,11 @@ public class OfficeManager extends Warehouse {
         }
     }
 
+    /**
+     * this method alerts the office manager when there are part/s that are near or below their minimum quantity and need
+     * to be restocked in the warehouse
+     * @param minimumQuantity
+     */
     public void orderPartsAlert(int minimumQuantity) {//need an additional method that notifies all parts that are at/below minimum qty
         //this method should be used in conjunction with sellpart
         for (int i = 0; bikeParts.size() > i; i++) {
@@ -59,10 +64,58 @@ public class OfficeManager extends Warehouse {
             if (currentPart.getQuantity() <= currentPart.getMinimumQuantity() + 1) {
                 System.out.println("The quantity is getting low for this part: " + currentPart.getPartName() + " " + "The current quantity is: " + currentPart.getQuantity() + " " + "The minimum quantity is: " + currentPart.getMinimumQuantity());
             }
-
         }
     }
 
+    /**
+     * This method allows the office manager to order new part/s when they are near or below their minimum quantity
+     * @return
+     */
+    public static BikePart orderParts(){
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Do you want to order parts for the warehouse? ");
+        String choice = userInput.next();
+        if(choice.equals("Y")){
+            System.out.print("How many parts would you like to order? ");
+            int newChoice = userInput.nextInt();
+            for(int i = 0; i < newChoice; i++){
+                String partName = "";
+                int partNumber = 0;
+                double listPrice = 0.0;
+                double salePrice = 0.0;
+                boolean onSale = false;
+                int quantity = 0;
+                int minimumQuantity = 0;
+                try {
+                    System.out.print("Please enter a part name: ");
+                    partName = userInput.next();
+                    System.out.print("Please enter a part number: ");
+                    partNumber = userInput.nextInt();
+                    System.out.print("Please enter a part list price: ");
+                    listPrice = userInput.nextDouble();
+                    System.out.print("Please enter a part sale price: ");
+                    salePrice = userInput.nextDouble();
+                    System.out.print("Please enter if part is on sale: ");
+                    onSale = userInput.nextBoolean();
+                    System.out.print("Please enter part quantity: ");
+                    quantity = userInput.nextInt();
+                    System.out.print("Please enter the minimum quantity you want in inventory: ");
+                    minimumQuantity = userInput.nextInt();
+                    return new BikePart(partName, partNumber, listPrice, salePrice, onSale, quantity, minimumQuantity);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            }
+        if(choice.equals("N")){
+            System.exit(0);
+        }
+        return new BikePart();
+    }
+
+    /**
+     * this method generates the commission of the salesperson that is chosen.
+     */
     public void salesCommissions() {
         SalesAssociate salesAssociate = new SalesAssociate();
         salesAssociate.commission();
@@ -70,6 +123,3 @@ public class OfficeManager extends Warehouse {
     }
 }
 
-
-//add order part to main menu, this will be the same as the enter a part except you need to be able to order multiple parts at a time
-//enter the number of parts you wish to order, enter the part
